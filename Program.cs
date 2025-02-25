e7fd2fee5fcbee2c5f38421a688d583366aa8498
Marius <blokma1124@gmail.com>
2025-02-25 18:46:37 +0100

    Changed Convert.ToInt32() to a safer method int.TryParse which is more user friendly

--- Program.cs
+++ Program.cs
@@ -4,24 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Temperatures are measured in Celsius and not Fahrenheit
         Console.Write("Enter the temperature: ");
-        int temperature = Convert.ToInt32(Console.ReadLine());
+        string input = Console.ReadLine() ?? "";
 
-        // If temperature is below 10 "It's cold"
-        if (temperature < 10)
+        if (int.TryParse(input, out int temperature))
         {
-            Console.WriteLine("It's cold!");
+            if (temperature < 10)
+            {
+                Console.WriteLine("It's cold!");
+            }
+            else if (temperature <= 25)
+            {
+                Console.WriteLine("It's warm!");
+            }
+            else
+            {
+                Console.WriteLine("It's hot!");
+            }
         }
-        // If temperature is above 10 or below 25 "It's warm"
-        else if (temperature >= 10 && temperature <= 25)
-        {
-            Console.WriteLine("It's warm!");
-        }
-        // or else above 25 celsius "It's hot"
         else
         {
-            Console.WriteLine("It's hot!");
+            Console.WriteLine("Invalid input. Please enter a valid number.");
         }
     }
 }
